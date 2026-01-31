a19001f6f8a5a292dc62c555670835643a681fca
Hrishi <rathodhrishi12345@gmail.com>
2026-01-31 16:27:09 +0530

    header formating

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -2,16 +2,14 @@ import Link from "next/link";
 
 export default function Header(){
   return(
-    <header className="w-full bg-[#fbf7f2]">
-      <div className="mx-auto max-w-[1400px] px-10 py-8 flex items-center justify-between">
+    <header className="relative z-10 w-full bg-[#fbf7f2]">
+      <div className="mx-auto max-w-[1400px] px-10 py-10 flex items-center justify-between">
 
-        <div className="text=[20px] font-medium texxt-[#223614]">
-          <Link href="/" className="text-xl font-serif tracking-wide text-[#223614]">Lilac Template</Link>
-          <nav className="flex gap-8 text-[16px] text-[#223614]">
+          <Link href="/" className="text-[20px] font-serif tracking-wide text-[#223614]">Lilac Template</Link>
+          <nav className="flex gap-10 text-[16px] text-[#223614]">
             <Link href="/blog" className="hover:opacity-70 transition">Blog</Link>
             <Link href="/contact" className="hover:opacity-70 transition">Contact</Link>
           </nav>
-        </div>
       </div>
     </header>
   );
