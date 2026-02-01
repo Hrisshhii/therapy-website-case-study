7778074f8eec6417010b60c6439462f8dde3b6e2
Hrishi <rathodhrishi12345@gmail.com>
2026-02-01 21:13:39 +0530

    Starting Contact Section

--- app/contact/contact-components/Contact-primary.tsx
+++ app/contact/contact-components/Contact-primary.tsx
@@ -2,23 +2,22 @@
 
 export default function ContactPrimary() {
   return (
-    <section className="pt-40 pb-32 bg-accent-soft text-primary">
+    <section className="pt-40 sm:pb-32 pb-25 bg-accent-soft text-primary">
       <div className="mx-auto max-w-450 section-x">
-        <div className="grid grid-cols-1 lg:grid-cols-[1.2fr_1fr] gap-20 items-start">
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr_1.3fr] gap-16 lg:gap-20">
 
-          <div>
-            <h1 className="heading-xl mb-8">
+          <div className="flex flex-col items-center">
+            <h1 className="font-medium sm:text-left text-primary leading-[1.12] text-[clamp(2.2rem,8vw,3rem)] sm:text-[calc(4.2vw+1rem)]">
               Let&apos;s Connect
             </h1>
 
-            <p className="max-w-162.5 text-[1.15rem] leading-[1.6] mb-14">
-              Starting therapy is courageous.
-              <br />
-              Get in touch for questions, or to book a free 15-minute consultation.
-            </p>
+            <div className="mt-8 text-center sm:text-left sm:mt-25 text-primary text-[clamp(1rem,1.35vw,1.45rem)]">
+              <p className="mb-2">Starting therapy is courageous.</p>
+              <p>Get in touch for questions, or to book a free 15-minute consultation.</p>
+            </div>
 
-            <div className="relative mt-20 w-fit">
-              <div className="w-132.5 h-195 overflow-hidden rounded-t-full">
+            <div className="relative mt-14 lg:mt-20 mx-auto lg:mx-0">
+              <div className="w-42.5 h-69 sm:w-72 sm:h-105 overflow-hidden rounded-t-full">
                 <img
                   src="/about-main.jpg"
                   alt=""
@@ -26,7 +25,7 @@ export default function ContactPrimary() {
                 />
               </div>
 
-              <div className="absolute -bottom-12 -right-12 w-95 h-95 rounded-full overflow-hidden">
+              <div className="absolute bottom-0 -right-20 w-30 h-30 sm:-right-27 sm:w-45 sm:h-45 rounded-full overflow-hidden">
                 <img
                   src="/about-circle.jpg"
                   alt=""
@@ -36,14 +35,10 @@ export default function ContactPrimary() {
             </div>
           </div>
 
-          <div className="flex items-start justify-end">
-            <div className="bg-primary text-secondary p-10 max-w-200">
-              <p className="text-[1.05rem] leading-[1.7]">
-                <strong>PLEASE NOTE:</strong> If you opt to use a “Form Block” on
-                your contact page this is not HIPAA-compliant.
-                <br /><br />
-                Instead, you can embed a HIPAA-compliant form, a link to your
-                client portal, or simply put your email address.
+          <div className="flex justify-start lg:justify-end lg:items-center">
+            <div className="bg-primary text-accent-soft p-6 sm:p-8 lg:p-10 max-w-full lg:max-w-200">
+              <p className="text-[1rem] sm:text-[1.35rem] leading-[1.7] sm:pb-15">
+                <strong>PLEASE NOTE:</strong> If you opt to use a “Form Block” on your contact page this is not HIPAA-compliant. Squarespace stores data that is input into forms in the Marketing tab under Profiles. Instead, you can embed a HIPAA-compliant form, a link to your client portal, or simply put your email address.
               </p>
             </div>
           </div>
