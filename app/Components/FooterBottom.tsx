c8a2db1f3b3f97cecbd3fd94fed8782ad4946b0b
Hrishi <rathodhrishi12345@gmail.com>
2026-02-03 00:39:41 +0530

    Creating dummy data for the leagal section

--- app/Components/FooterBottom.tsx
+++ app/Components/FooterBottom.tsx
@@ -9,15 +9,15 @@ export default function FooterBottom() {
       <div className="mx-auto max-w-450 section-x py-10 text-center text-[0.95rem] text-primary">
         <Animated show={show} delay={120}>
           <div className="flex flex-wrap justify-center gap-x-6 gap-y-3 underline mb-6">
-            <a href="/privacy-policy" className="hover:underline text-black text-[calc(0.2vw+0.8rem)]">Privacy & Cookies Policy</a>
-            <a href="/good-faith-estimate" className="hover:underline text-black text-[calc(0.2vw+0.8rem)]">Good Faith Estimate</a>
-            <a href="/terms-conditions" className="hover:underline text-black text-[calc(0.2vw+0.8rem)]">Website Terms & Conditions</a>
-            <a href="/disclaimer" className="hover:underline text-black text-[calc(0.2vw+0.8rem)]">Disclaimer</a>
+            <a href="/legal/privacy-policy" className="hover:underline text-black text-[calc(0.2vw+0.8rem)]">Privacy & Cookies Policy</a>
+            <a href="/legal/good-faith-estimate" className="hover:underline text-black text-[calc(0.2vw+0.8rem)]">Good Faith Estimate</a>
+            <a href="/legal/terms-conditions" className="hover:underline text-black text-[calc(0.2vw+0.8rem)]">Website Terms & Conditions</a>
+            <a href="/legal/disclaimer" className="hover:underline text-black text-[calc(0.2vw+0.8rem)]">Disclaimer</a>
           </div>
         
           <p className="mb-4 text-[calc(0.2vw+0.8rem)]">
             Website Template Credits:{" "}
-            <a href="https://www.gobloomcreative.com/" className="underline text-black">Go Bloom Creative</a>
+            <a href="https://www.gobloomcreative.com/" target="_blank" className="underline text-black">Go Bloom Creative</a>
           </p>
         </Animated>
         <p className={`mt-20 sm:mt-30 text-[calc(0.2vw+0.8rem)] transition-opacity duration-700 ease-in-out
---
+++ app/legal/privacy-policy/page.tsx
@@ -0,0 +1,24 @@
+import Footer from "@/app/Components/Footer";
+import Link from "next/link";
+
+export default function PrivacyPolicy() {
+  return (
+    <section className="bg-secondary text-primary py-32">
+      <div className="mx-auto max-w-450 section-x body-md text-primary">
+        <p>We couldn&apos;t find the page you were looking for. This is either because:</p>
+        <ul className="list-disc pl-6 my-4">
+          <li className="ml-10">There is an error in the URL entered into your web browser. Please check the URL and try again.</li>
+          <li className="ml-10">The page you are looking for has been moved or deleted.</li>
+        </ul>
+        <p className="mt-6">
+          You can return to our homepage by 
+          <Link href="/" className="underline">clicking here</Link>
+          , or you can try searching for the content you are seeking by 
+          <Link href="/search" className="underline">clicking here</Link>
+          .
+        </p>
+      </div>
+      <Footer />
+    </section>
+  );
+}
