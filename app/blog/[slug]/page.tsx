789ace011f5e2774b1a45eb52ef5c6f11083bc81
Hrishi <rathodhrishi12345@gmail.com>
2026-02-03 05:33:49 +0530

    Disable Netlify Next.js runtime, use static export

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,12 @@ import { posts } from "../blog-components/data";
 import { notFound } from "next/navigation";
 import BlogPostClient from "./components/BlogPostClient";
 
+export async function generateStaticParams() {
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
 type Props = {
   params: Promise<{ slug: string }>;
 };
